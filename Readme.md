8fd74d0ebb104bfc3974a48a94f8ef90127724a4
napookooo <napookooo4789@gmail.com>
2025-03-06 20:33:56 +0700

    math

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Basic ai 
+
+x*w + b = y -> 1/1+e^-y = z 
+
+
+(input * weights) + bias -> sigmoid fn -> output
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -14,6 +15,19 @@
 
 float cat_train[TRAIN_COUNT][IMAGE_WIDTH*IMAGE_HEIGHT*IMAGE_CHANNELS] = {0};
 float dog_train[TRAIN_COUNT][IMAGE_WIDTH*IMAGE_HEIGHT*IMAGE_CHANNELS] = {0};
+
+//                            weights          inputs   size
+float dot_product(const float *xs, const float *ys, int n) {
+  float sum = 0;
+  for (int i=0; i<n; i++) {
+    sum += xs[i] * ys[i];
+  }
+  return sum;
+}
+
+float sigmoid(float x){
+  return 1.0f/ (1.0 + expf(-x));
+}
 
 void load_image(const char *filename, float *image){
 
@@ -32,17 +46,52 @@ void load_dataset(const char *path, const char *label, int count, float train[][
   }
 }
 
+typedef struct{
+  float weights_input[IMAGE_WIDTH*IMAGE_HEIGHT*IMAGE_CHANNELS];
+  float bias_input;
+} neural_network;
+
+void nn_init(neural_network *nn){
+  for(int i=0; i<IMAGE_WIDTH*IMAGE_HEIGHT*IMAGE_CHANNELS; i++){
+    nn->weights_input[i] = (rand() / (float)RAND_MAX -0.5f) * 2.0;
+  }  
+  nn->bias_input = (rand() / (float)RAND_MAX -0.5f) * 2.0;
+}
+
+float feed_forward(const neural_network *nn, float *x){
+  float z = dot_product(nn->weights_input, x, IMAGE_WIDTH*IMAGE_HEIGHT*IMAGE_CHANNELS) + nn->bias_input;
+
+  float y = sigmoid(z);
+
+  return y;
+}
+
 int main(int argc, char *argv[]){
+
+  srand(time(NULL));
+
+  neural_network nn;
+  nn_init(&nn);
 
   load_dataset("dvc/train/train","cat", TRAIN_COUNT, cat_train);
   load_dataset("dvc/train/train","dog", TRAIN_COUNT, dog_train);
 
-  float x = 1.2;
+  int total = 2*TRAIN_COUNT;
+  int correct = 0;
+  for (int i=0; i<TRAIN_COUNT; i++){
+    float prediction = feed_forward(&nn, cat_train[i]);
 
-  float w, b;
-  float y = w*x + b;
+    if (prediction >= 0.5){
+      correct++;
+    }
 
-  printf("%f", y);
+    prediction = feed_forward(&nn, dog_train[i]);
+    if (prediction <= 0.5){
+      correct++;
+    }
+  }
+
+  printf("%f\n", correct/(float)total);
 
   return 0;
 }
